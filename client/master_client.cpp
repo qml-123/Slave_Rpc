fd8546f00fbe52f60e2e16d94a0b41866c9d18f5
qml-123 <2857820518@qq.com>
2022-06-06 10:02:16 +0800

    pool

---
+++ client/master_client.cpp
@@ -0,0 +1,46 @@
+#include "../gen-cpp/Master.h"
+#include <iostream>
+#include <thrift/transport/TSocket.h>
+#include <thrift/transport/TBufferTransports.h>
+#include <thrift/protocol/TBinaryProtocol.h>
+using namespace apache::thrift;
+using namespace apache::thrift::protocol;
+using namespace apache::thrift::transport;
+
+using namespace ::rpc::master;
+using boost::shared_ptr;
+
+
+//client连master
+int main(int argc, char **argv) {
+    //master地址
+    ::apache::thrift::stdcxx::shared_ptr<TSocket> socket(new TSocket("121.40.40.117", 9090)); //注意此处的ip和端口
+    ::apache::thrift::stdcxx::shared_ptr<TTransport> transport(new TFramedTransport(socket));
+    ::apache::thrift::stdcxx::shared_ptr<TProtocol> protocol(new TBinaryProtocol(transport));
+    transport->open();
+    
+    MasterClient client(protocol);
+    
+    SetRequest setRequest;
+    SetResponse setResponse;
+    setRequest.key = "b";
+    setRequest.value = "qmlqml";
+    client.Set(setResponse, setRequest);
+    std::cout << "setResponse:" + setResponse.message << std::endl;
+    
+    GetRequest getRequest;
+    GetResponse getResponse;
+    //client发出Get请求，发送key
+    getRequest.key="b";
+    client.Get(getResponse, getRequest);
+    std::cout << "getResponse:[message]:" + getResponse.message << " " << "[key]= " + getRequest.key << " value=" << getResponse.value << std::endl;
+    
+    DelRequest delRequest;
+    DelResponse delResponse;
+    delRequest.key = "b";
+    client.Del(delResponse, delRequest);
+    std::cout << "delResponse:" + delResponse.message << std::endl;
+    
+    transport->close();
+    return 0;
+}
